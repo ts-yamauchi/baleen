13dffc41638cfea99a8d723ba3c72897cdc1200a
Yamauchi Tsubasa <ts-yamauchi@respect-pal.jp>
2016-08-09 20:46:36 +0900

    mainレイアウトを作成

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('title', 'TOP')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1 style="text-align: center; font-size: 200px;">Baleen</h1>
+            <form>
+                <div class="form-group">
+                    <input type="text" class="form-control">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Baleen - @yield('title')</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+    @include('layouts.navbar')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
