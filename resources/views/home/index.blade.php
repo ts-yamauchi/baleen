2ade112bc83a1d262acd92f8ffc95df70e08223c
ts-yamauchi <ts.yamauchi.rp@gmail.com>
2016-08-10 01:29:33 +0900

    TOPページビューを微修正

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,12 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h1 style="text-align: center; font-size: 200px;">Baleen</h1>
+            <h1 style="text-align: center; font-size: 200px; font-family: 'Ubuntu', sans-serif; margin-top: 300px;">Baleen</h1>
+        </div>
+        <div class="col-md-4" ></div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
             <form>
                 <div class="form-group">
                     <input type="text" class="form-control">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,10 @@
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
+
 </head>
 <body>
     @include('layouts.navbar')
